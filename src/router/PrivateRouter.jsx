7818daf02ad88938edba34f7a9d52dd324877fff
BruceHill <brucehillwalley@gmail.com>
2023-12-24 00:48:32 +0300

    first commit

---
+++ src/router/PrivateRouter.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Navigate, Outlet } from 'react-router-dom'
+import Navbar from '../components/Navbar'
+
+const PrivateRouter = () => {
+  return true ? (
+    <>
+      <Navbar/>
+      <Outlet/>
+    </>
+  ) :
+  (
+    <Navigate to="/" />
+  )
+}
+
+export default PrivateRouter
