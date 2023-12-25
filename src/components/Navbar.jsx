8b9401ec42e9c030d006d31bcc56ce5cbab84c72
BruceHill <brucehillwalley@gmail.com>
2023-12-25 21:43:59 +0300

    getData and search

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,8 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import logo from "../assets/logo.png";
 import { closeNavbar, openNavbar, logoutIcon } from "../helper/icons";
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
+import AuthProvider, { AuthContext } from "../context/AuthProvider";
 const navigation = [
   {
     title: "Home",
@@ -19,6 +20,9 @@ const navigation = [
 
 const Navbar = () => {
   const [show, setShow] = useState(false);
+  const {logout}=useContext(AuthContext)
+  const location =useLocation()
+  console.log(location);
 
   return (
     <nav className="bg-navbarColor md:text-sm">
@@ -54,7 +58,7 @@ const Navbar = () => {
               >
                 <NavLink
                   to={item.path}
-                  className={`block hover:bg-main rounded-full py-2 px-4 hover:text-white`}
+                  className={`block hover:bg-main rounded-full py-2 px-4 hover:text-white ${location.pathname===item.path ? "underline scale-150" :""}`}
                 >
                   {item.title}
                 </NavLink>
@@ -64,6 +68,7 @@ const Navbar = () => {
           <div className="flex-1 gap-x-6 items-center justify-end mt-6 space-y-6 md:flex md:space-y-0 md:mt-0">
             <NavLink
               to="/"
+              onClick={logout}
               className="flex items-center justify-center gap-x-1 py-2 px-4 font-medium text-gray-700 hover:bg-main hover:text-white active:bg-gray-900 rounded-full md:inline-flex" 
             >
               Logout {logoutIcon}
--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -1,11 +1,11 @@
 //AuthContext
 
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 export const AuthContext = createContext();
 
 const AuthProvider = ({ children }) => {
-  const [user, setUser] = useState("");
+  const [user, setUser] = useState(JSON.parse(sessionStorage.getItem("user")) || null);
   const navigate = useNavigate();
 
   const login = (info) => {
@@ -13,8 +13,17 @@ const AuthProvider = ({ children }) => {
     navigate("/dashboard");
   };
 
+  const logout = ()=>{
+    setUser(null)
+
+  }
+
+  useEffect(() => {
+    sessionStorage.setItem("user", JSON.stringify(user));
+  },[user]) //!componenentDidUpdate
+
   return (
-    <AuthContext.Provider value={{ user, login }}>
+    <AuthContext.Provider value={{ user, login, logout }}>
       {children}
     </AuthContext.Provider>
   );
